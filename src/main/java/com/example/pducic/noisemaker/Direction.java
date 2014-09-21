17a6ee6d418e0a9206ef77bd543a6214a4339c12
pducic <petar.ducic@infobip.com>
2014-09-21 17:52:55 +0200

    broken commit

---
+++ src/main/java/com/example/pducic/noisemaker/Direction.java
@@ -0,0 +1,28 @@
+package com.example.pducic.noisemaker;
+
+/**
+ * Created by pducic on 21.09.14..
+ */
+public enum Direction {
+
+    X(0),
+    Y(1),
+    Z(2);
+
+    private int index;
+
+    Direction(int index) {
+        this.index = index;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public static Direction fromValue(int index) {
+        for (Direction d : values()) {
+            if (d.index == index) return d;
+        }
+        return null;
+    }
+}
